1c290b94459086b7adc22ff1fb6a932156f3492c
mE-uMAr <me.umar0027@gmail.com>
2025-03-07 06:33:54 +0500

    fixed bugs

--- employ/admin.py
+++ employ/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,Tasks,Accounts
+from .models import Profile,Tasks,Accounts,AddEmployee
 
 admin.site.register(Profile)
 admin.site.register(Tasks)
 admin.site.register(Accounts)
+admin.site.register(AddEmployee)
